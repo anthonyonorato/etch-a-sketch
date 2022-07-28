4fdc5b2f1f696f22e6caf7a05b5182fedc4d22ef
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-28 15:46:22 -0400

    Set variables in js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const container = document.getElementById("container");
 const sizeValue = document.getElementById("size-value");
 const sizeSlider = document.getElementById("size-slider");
 const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
-const clearButton = document.getElementById("clear");
+const clearButton = document.getElementById("clear"):
