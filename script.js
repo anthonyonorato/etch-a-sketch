ff8765205f7b539a47107e9c1f71e4239e5e49f5
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-28 15:46:45 -0400

    updated js vars

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const sizeValue = document.getElementById("size-value");
 const sizeSlider = document.getElementById("size-slider");
 const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
 const clearButton = document.getElementById("clear"):
+
+let colorChoice = 'black';
+let gridItems = Array.from(document.querySelectorAll('.grid-item'));
