f594e705c85a7179d698431f2e1b7bbec07b75ca
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-28 15:51:25 -0400

    add function for making a grid

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const clearButton = document.getElementById("clear"):
 
 let colorChoice = 'black';
 let gridItems = Array.from(document.querySelectorAll('.grid-item'));
+
+function makeGrid(rows, columns, color) {
+  
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-colums', columns);
+    
+    for (i = 0; i < (rows * columns); i++) {
+      let cell = document.createElement('div');
+      container.appendChild(cell).className = 'grid-item';
+    };
+  
+    gridItems = Array.from(document.querySelectorAll('.grid-item'));
+  
+    checkColor();
+  
+  };
