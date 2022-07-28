ed3344a19d0ddfc9709323f879f16c3bc209bc3e
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-28 16:40:13 -0400

    add functions for clearing and sizing

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const container = document.getElementById("container");
 const sizeValue = document.getElementById("size-value");
 const sizeSlider = document.getElementById("size-slider");
 const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
-const clearButton = document.getElementById("clear"):
+const clearButton = document.getElementById("clear");
 
 let colorChoice = 'black';
 let gridItems = Array.from(document.querySelectorAll('.grid-item'));
 
 function makeGrid(rows, columns, color) {
   
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-colums', columns);
-    
-    for (i = 0; i < (rows * columns); i++) {
-      let cell = document.createElement('div');
-      container.appendChild(cell).className = 'grid-item';
-    };
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-colums', columns);
   
-    gridItems = Array.from(document.querySelectorAll('.grid-item'));
-  
-    checkColor();
-  
+  for (i = 0; i < (rows * columns); i++) {
+    let cell = document.createElement('div');
+    container.appendChild(cell).className = 'grid-item';
+  };
+
+  gridItems = Array.from(document.querySelectorAll('.grid-item'));
+
+  checkColor();
+
 };
 
 function removeAllChildren(parent) {
@@ -38,22 +38,44 @@ function randomRGB() {
 }
 
 function checkColor() {
-    if (colorChoice === 'black') {
-      gridItems.forEach((gridItem) =>
-        gridItem.addEventListener('mouseover',
-          e => {
-            e.target.style.backgroundColor = 'black';
-          }
-        )
+  if (colorChoice === 'black') {
+    gridItems.forEach((gridItem) =>
+      gridItem.addEventListener('mouseover',
+        e => {
+          e.target.style.backgroundColor = 'black';
+        }
       )
-    } else if (colorChoice === 'chaos') {
-      gridItems.forEach((gridItem) =>
-        gridItem.addEventListener('mouseover',
-          e => {
-            e.target.style.backgroundColor = randomRGB();
-          }
-        )
+    )
+  } else if (colorChoice === 'chaos') {
+    gridItems.forEach((gridItem) =>
+      gridItem.addEventListener('mouseover',
+        e => {
+          e.target.style.backgroundColor = randomRGB();
+        }
       )
-    }
+    )
   }
-  
+}
+
+makeGrid(16, 16, 'black');
+
+colorChoices.forEach((color) =>
+  color.onclick = (e) => {
+    colorChoice = e.target.value;
+    checkColor();
+  })
+
+clearButton.onclick = function() {
+  for (var gridItem of gridItems) {
+    gridItem.style.backgroundColor = '';
+  }
+}
+
+sizeSlider.oninput = function() {
+  sizeValue.innerText = `${this.value} x ${this.value}`;
+  removeAllChildren(container);
+  makeGrid(this.value, this.value, colorChoice);
+};
+
+
+
