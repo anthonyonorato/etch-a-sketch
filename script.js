d451f2326fc21316dea7d2a06b9b92aebb3a2325
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-28 15:45:54 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container");
+const sizeValue = document.getElementById("size-value");
+const sizeSlider = document.getElementById("size-slider");
+const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
+const clearButton = document.getElementById("clear");
